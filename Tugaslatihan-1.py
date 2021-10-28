50eea9aad50a005a0c05969e7cdd8ac89953a319
Rio Saputra <riobadrun1721@gmail.com>
2021-10-28 09:40:21 +0700

    Biodata diri pyhton

---
+++ Tugaslatihan-1.py
@@ -0,0 +1,9 @@
+Nama =str("Rio Saputra")
+Umur =str("20")
+Panggilan = str("Rio")
+Tempatlahir = str("Bekasi")
+alamat = str("Kp.Bulak kunyit cibitung Bekasi")
+telepon = str("085751281103")
+
+print("Assalamu'alaikum")
+print("Let me introduce my self.my name is",Nama,",but you can call me",Panggilan,". I was born in",Tempatlahir," and i am",Umur,"years old. I am very gladd if you want to invite my house in",alamat,". so dont forget to call e before with the number ",telepon,"")

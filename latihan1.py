a6fd4a2f7f1c3042f8254d3a825e245b84739b86
Rio Saputra <riobadrun1721@gmail.com>
2021-10-28 09:32:38 +0700

    Latihan1 pyhton

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("hello")
+print("saya sedang belajar pyhton")
+
+a = 8
+b = 6
+print ("variabel a=",a)
+print ("variabel b=",b)
+print ("hasil penjumalahan a+b=",a+b)
+
+a=input("masukan nilai a:8")
+b=input("masukan nilai b:6")
+print("variabel a=",8)
+print("variabel b=",6)
+print("hasil penggabungan {1}&{0}=%d".format(8,6) %(8+6))
+
+#konversi nilai variabel
+a=str(a)
+b=str(b)
+print("hasil penjumlahan {1}+{0}=%d".format(8,6) %(8+6))
+print("hasil pembagian {1}/{0}=%f".format(8,6) %(8/6))
